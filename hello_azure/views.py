64940dd41b8611eb6d4b6a71135de71b38918c8c
dbohn94 <davidwbohn@gmail.com>
2023-12-09 03:06:04 +0000

    adding comments to views.py file

--- hello_azure/views.py
+++ hello_azure/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+#Index view: URL Endpoint index, HTTP method (GET by default)
+#Behavior: prints a message to the console indicating that a request for the index page has been recieved and then renders an HTML template named 'hello_azure/index.html' using the render function.
+#The rendered HTML is then sent as the HTTP response 
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
 
+#hello view: handles both GET and POST requests 
+#Behavior: POST, retrieves the value of the 'name' parameter from the POST data
 @csrf_exempt
 def hello(request):
     if request.method == 'POST':
