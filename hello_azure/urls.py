316815e5e7db5790560aea168a145b88c0a47fa8
dbohn94 <davidwbohn@gmail.com>
2024-01-23 02:29:19 +0000

    202401 Updates

--- hello_azure/urls.py
+++ hello_azure/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('hello', views.hello, name='hello'),
-    #path('spy_chart', views.spy_chart, name='spy_chart'),
-    path('templates/hello_azure/', views.spy_chart, name='spy_chart.html')
+    path('bot', views.bot, name='bot.html'),
+    path('spy_chart', views.spy_chart, name='spy_chart.html')
     
 ]
--- hello_azure/views.py
+++ hello_azure/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-#import matplotlib.pyplot as plt
 import yfinance as yf
 from django.http import FileResponse
 from io import BytesIO
@@ -13,6 +12,11 @@ import plotly.graph_objects as go
 import pandas as pd 
 import pytz
 import pandas_market_calendars as mcal
+import threading
+import json 
+from hello_azure import helpers
+from alpaca.trading.enums import OrderSide
+import time
 
 #Index view: URL Endpoint index, HTTP method (GET by default)
 #Behavior: prints a message to the console indicating that a request for the index page has been recieved and then renders an HTML template named 'hello_azure/index.html' using the render function.
@@ -20,6 +24,44 @@ import pandas_market_calendars as mcal
 def index(request):
     print('Request for index page received')
     return render(request, 'hello_azure/index.html')
+
+
+def bot_logic():
+        config = json.loads(open("hello_azure/config.json").read())
+        data = helpers.get_historical_price(config["Stock"],config["Key"],config["Secret"])
+        response = ""
+        #open_positions = helpers.get_open_position(config["Key"],config["Secret"],config["Stock"]).symbol
+        #print(f"Open positions: {open_positions}")
+
+        if config["Stock"] not in helpers.get_open_position(config["Key"],config["Secret"],config["Stock"]).symbol:
+            while True:
+                sma1 = helpers.calculate_sma(data, config["SMA_1"])
+                sma2 = helpers.calculate_sma(data, config["SMA_2"])
+                
+                if sma1 > sma2:
+                    order = helpers.place_market_order(config["Key"],config["Secret"],config["Stock"], config["Qty"], OrderSide.BUY)
+                    print(order)
+                elif sma2 > sma1:
+                    order = helpers.place_market_order(config["Key"],config["Secret"],config["Stock"], config["Qty"], OrderSide.SELL)
+                    print(order)
+                else:
+                    print(F"No Crossover yet, Current SMA {config['SMA_1']}: {sma1}, SMA {config['SMA_2']}: {sma2}")
+                
+                curr_price = helpers.get_price(config["Key"],config["Secret"],config["Stock"])
+                data.loc[len(data)] = [0,0,0,curr_price,0,0,0,0,0]
+        else:
+            print((f"Already have an open position in: "
+                f"{helpers.get_open_position(config['Key'],config['Secret'],config["Stock"]).symbol} "
+                f"| qty= "
+                f"{helpers.get_open_position(config['Key'],config['Secret'],config["Stock"]).qty} ")) 
+            time.sleep(300)
+        return response
+
+
+def bot(request):
+    response = bot_logic() # Run the bot_logic function
+    return render(request, 'hello_azure/bot.html', {'response': response})  # Pass the result to the template
+
 
 #hello view: handles both GET and POST requests 
 #Behavior: POST, retrieves the value of the 'name' parameter from the POST data
