547af8001c8f6a694a7b7c71b8b2f45775884dd2
Christopher Thorne <libcthorne@gmail.com>
2024-04-19 16:36:40 +0100

    Simplify startup command

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 python manage.py migrate
-celery -A quickstartproject.celery worker --queues=jobs --logfile celery.log &
+celery -A quickstartproject worker --logfile celery.log &
 celery -A quickstartproject beat -l info -S django --logfile celery-beat.log &
 gunicorn --bind=0.0.0.0 --timeout 600 quickstartproject.wsgi
