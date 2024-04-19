d5ec2c17d814dab9c22c3b5427f3f5c972b96e09
Christopher Thorne <libcthorne@gmail.com>
2024-04-19 14:18:38 +0100

    Use reliable source of host domain

--- quickstartproject/production.py
+++ quickstartproject/production.py
@@ -3,6 +3,6 @@ import os
 
 # Configure the domain name using the environment variable
 # that Azure automatically creates for us.
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
+ALLOWED_HOSTS = [os.environ['APPLICATION_DOMAIN']] if 'APPLICATION_DOMAIN' in os.environ else []
 
 DEBUG = False
