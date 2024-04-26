51317944e0eb141d77f5bb7de29d4c29f101f295
Christopher Thorne <libcthorne@gmail.com>
2024-04-26 12:30:51 +0100

    Use postgres on prod

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -79,13 +79,24 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# TODO: Move to PostgreSQL
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get("ENVIRONMENT") == "PROD":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ["POSTGRES_DB"],
+            'USER': os.environ["POSTGRES_USER"],
+            'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+            'HOST': os.environ["POSTGRES_HOST"],
+            'PORT': '',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
