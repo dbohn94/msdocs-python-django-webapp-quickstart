646eeec007d49a520ff6615451d9845a51542bed
Christopher Thorne <libcthorne@gmail.com>
2024-04-26 12:55:28 +0100

    Fix CSRF error on prod

--- quickstartproject/production.py
+++ quickstartproject/production.py
@@ -5,4 +5,17 @@ import os
 # that Azure automatically creates for us.
 ALLOWED_HOSTS = [os.environ['APPLICATION_DOMAIN']] if 'APPLICATION_DOMAIN' in os.environ else []
 
+CSRF_TRUSTED_ORIGINS = [os.environ['APPLICATION_DOMAIN']] if 'APPLICATION_DOMAIN' in os.environ else []
+
 DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ["POSTGRES_DB"],
+        'USER': os.environ["POSTGRES_USER"],
+        'PASSWORD': os.environ["POSTGRES_PASSWORD"],
+        'HOST': os.environ["POSTGRES_HOST"],
+        'PORT': '',
+    }
+}
--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -79,24 +79,12 @@ WSGI_APPLICATION = 'quickstartproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if os.environ.get("ENVIRONMENT") == "PROD":
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': os.environ["POSTGRES_DB"],
-            'USER': os.environ["POSTGRES_USER"],
-            'PASSWORD': os.environ["POSTGRES_PASSWORD"],
-            'HOST': os.environ["POSTGRES_HOST"],
-            'PORT': '',
-        }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
